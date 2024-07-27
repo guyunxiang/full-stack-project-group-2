2771eb6434ed3572c3f4f298806b484a41159893
guyunxiang <guyunxiang32@gmail.com>
2024-07-26 23:29:48 -0400

    add new router

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 import EmployeeListPage from './pages/EmployeeList';
 import EmployeeCreatePage from './pages/EmployeeCreate';
 import EmployeeDetailPage from './pages/EmployeeDetail';
+import UpComingRetirement from './pages/UpComingRetirement';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<EmployeeListPage />} />
           <Route path="/add" element={<EmployeeCreatePage />} />
           <Route path="/employee/:id" element={<EmployeeDetailPage />} />
+          <Route path="/retirement" element={<UpComingRetirement />} />
         </Routes>
       </main>
       <Footer />
