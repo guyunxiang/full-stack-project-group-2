86fb32efb16091d4baa85253c5ee6082920442c8
guyunxiang <guyunxiang32@gmail.com>
2024-07-26 23:31:18 -0400

    new page template

---
+++ ui/src/pages/UpComingRetirement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UpComingRetirement() {
+  return (
+    <div className='container'>
+      <h2>UpComing Retirement</h2>
+      <hr />
+    </div>
+  );
+}
+
+export default UpComingRetirement;
