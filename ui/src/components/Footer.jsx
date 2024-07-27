2e0727a8aaaa76f79605ed6e3280f9450181b888
guyunxiang <guyunxiang32@gmail.com>
2024-07-26 23:29:59 -0400

    update text

--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-3 text-center">
-      <p className="mb-0 fs-6">Powered By Yunxiang Gu - 8904492</p>
+      <p className="mb-0 fs-6">Powered By Group 2</p>
     </footer>
   )
 }
