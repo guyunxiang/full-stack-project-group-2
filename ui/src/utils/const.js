4d9afdce32929fbffdff5c247790450e11b6cae7
guyunxiang <guyunxiang32@gmail.com>
2024-07-29 17:41:15 -0400

    add status config

--- ui/src/utils/const.js
+++ ui/src/utils/const.js
@@ -4,3 +4,16 @@ export const TITLES = ["Employee", "Manager", "Director", "VP"];
 export const DEPARTMENTS = ["IT", "MARKETING", "HR", "Engineering"];
 // define employee type
 export const EMPLOYEETYPES = ["FullTime", "PartTime", "Contract", "Seasonal"];
+
+// 1 = Active
+// 2 = On Leave
+// 3 = Sick Leave
+// 4 = Suspended
+// 5 = Terminated
+export const STATUS = {
+  1: "Active",
+  2: "On Leave",
+  3: "Sick Leave",
+  4: "Suspended",
+  5: "Terminated"
+};
