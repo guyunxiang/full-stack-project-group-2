f710871dab40d6b4dc1cc2114f1768cba0f9e782
guyunxiang <guyunxiang32@gmail.com>
2024-07-29 11:28:04 -0400

    complete retirements page

--- ui/src/pages/UpComingRetirement.jsx
+++ ui/src/pages/UpComingRetirement.jsx
@@ -1,12 +1,136 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-function UpComingRetirement() {
+import EmployeeFilter from "../components/EmployeeFilter";
+
+const UpComingRetirement = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const employeeType = queryParams.get("employeeType");
+
+  const [employeeData, setEmploeeData] = useState([]);
+
+  // fetch employee data
+  const fetchEmployeeList = async (employeeType) => {
+    const requestBody = {
+      query: `
+        query GetEmployees($type: String) {
+          upcomingRetirements(type: $type) {
+            id
+            firstname
+            lastname
+            dob
+            doj
+            retirementDate
+            title
+            department
+            type
+            status
+          }
+        }
+      `,
+      variables: {
+        type: employeeType || null,
+      },
+    };
+
+    // query employee data with graphql
+    const res = await fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch employee list");
+    }
+    const data = await res.json();
+    // update employee data
+    setEmploeeData(data.data.upcomingRetirements);
+  };
+
+  useEffect(() => {
+    fetchEmployeeList(employeeType);
+  }, [employeeType]);
+
+  const handleChange = (value) => {
+    if (value === "All") {
+      navigate("/retirements");
+      return;
+    }
+    navigate("/retirements?employeeType=" + value);
+  };
+
+  // render each column data
+  const renderTd = (id, rowData) => {
+    const tds = Object.keys(rowData).map((key) => {
+      // format date of joining data
+      if (key === 'dob') {
+        const age = new Date().getFullYear() - new Date(+rowData[key]).getFullYear();
+        const dateString = new Intl.DateTimeFormat("en-CA").format(
+          new Date(+rowData[key])
+        );
+        return (
+          <React.Fragment key={rowData[key]}>
+            <td key={rowData[key]}>{age}</td>
+            <td key={`${rowData[key]}-dob`}>{dateString}</td>
+          </React.Fragment>
+        );
+      }
+      if (key === 'doj' || key === 'retirementDate') {
+        const dateString = new Intl.DateTimeFormat("en-CA").format(
+          new Date(+rowData[key])
+        );
+        return <td key={rowData[key]}>{dateString}</td>;
+      }
+      return <td key={rowData[key]}>{rowData[key]}</td>;
+    });
+    return tds;
+  };
+
+  // render each row data
+  const renderTable = (tableData) => {
+    // if no data, render "no data"
+    if (!tableData) {
+      return (
+        <tr>
+          <td colSpan="10" className="text-center text-secondary">
+            No data
+          </td>
+        </tr>
+      );
+    }
+    return tableData.map(({ id, ...item }) => (
+      <tr key={id}>{renderTd(id, item)}</tr>
+    ));
+  };
   return (
-    <div className='container'>
-      <h2>UpComing Retirement</h2>
+    <div className="EmployeeTable container">
+      <h2>UpComing Retirement Employee List</h2>
       <hr />
+      <EmployeeFilter handleChange={handleChange} />
+      <hr />
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>DOB</th>
+            <th>DOJ</th>
+            <th>Retirement Date</th>
+            <th>Title</th>
+            <th>Department</th>
+            <th>EmployeeType</th>
+            <th>CurrentStatus</th>
+          </tr>
+        </thead>
+        <tbody>{renderTable(employeeData)}</tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default UpComingRetirement;
