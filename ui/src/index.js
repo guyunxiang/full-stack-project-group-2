6e66584525829e2373b053f298cb7665da08bbfd
guyunxiang <guyunxiang32@gmail.com>
2024-08-14 20:55:36 -0400

    import index.css

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
 import App from './App';
 
